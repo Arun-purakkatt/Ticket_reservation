4e195416f1e0cc03e8dbbf4ca1c296e05c8f6237
Arun-purakkatt <55660573+Arun-purakkatt@users.noreply.github.com>
2020-01-27 14:47:20 +0530

    Add files via upload

---
+++ Ticket.py
@@ -0,0 +1,60 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class Ticket:\n",
+    "\n",
+    "    def setChildTicketPrice(self,child):\n",
+    "        self.child = child\n",
+    "\n",
+    "    def getChildTicketPrice(self):\n",
+    "        return self.child\n",
+    "\n",
+    "    def setAdultTicketPrice(self,adult ):\n",
+    "        self.adult = adult\n",
+    "\n",
+    "    def getAdultTicketPrice(self):\n",
+    "        return self.adult\n",
+    "\n",
+    "    def setSeniorTicketPrice(self, senior):\n",
+    "        self.senior = senior\n",
+    "\n",
+    "    def getSeniorTicketPrice(self):\n",
+    "        return self.senior\n",
+    "\n",
+    "  \n",
+    "    def setTotal(self,total):\n",
+    "        self.total = total\n",
+    "\n",
+    "\n",
+    "    def getTotal(self):\n",
+    "        return self.total"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
---
+++ checkout.py
@@ -0,0 +1,59 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import re\n",
+    "class Checkout:\n",
+    "\n",
+    "    def setCardNumber(self,cardno):\n",
+    "        self.cardno = cardno\n",
+    "\n",
+    "    def getCardNumber(self):\n",
+    "        return self.cardno\n",
+    "\n",
+    "    def setName(self,name):\n",
+    "        self.name = name\n",
+    "\n",
+    "    def getName(self):\n",
+    "        return self.name\n",
+    "\n",
+    "    def setExpirydate(self,expirydate):\n",
+    "        self.expirydate = expirydate\n",
+    "\n",
+    "    def getExpirydate(self):\n",
+    "        return self.expirydate\n",
+    "\n",
+    "    def setCVV(self,cvv):\n",
+    "        self.cvv = cvv\n",
+    "\n",
+    "    def getCVV(self):\n",
+    "        return self.cvv "
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
